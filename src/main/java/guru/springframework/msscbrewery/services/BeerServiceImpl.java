68135f0d00bd3fb41bfd3bc2cd0b63ec392419ab
phpilatso <phenyopilatso@momentum.co.za>
2020-10-03 22:34:10 +0200

    complete crud operations

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -13,5 +15,23 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //todo impl - would add a real impl to update beer
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        //todo impl - would delete beer by Id
+        log.debug("deleting a beer...");
     }
 }
--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -12,5 +14,21 @@ public class CustomerServiceImpl implements CustomerService {
         return CustomerDto.builder().id(UUID.randomUUID())
                 .customerName("Phenyo")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl - would add a real impl to update customer
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        //todo impl - would delete customer by Id
+        log.debug("deleting a beer...");
     }
 }
